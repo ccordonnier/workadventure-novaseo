a1699f6004d135a9824eb1a8b5a34f63e694edca
ccordonnier <corentincordonnier@hotmail.fr>
2021-01-21 07:42:53 +0000

    Deploying to gh-pages from master @ e046cddc8f4563152a921cc392af9b61ec07da12 🚀

---
+++ chaise.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="chaise" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../../../../Program Files/Tiled/examples/sticker-knight/map/pushBlock1.png" width="96" height="96"/>
+</tileset>
